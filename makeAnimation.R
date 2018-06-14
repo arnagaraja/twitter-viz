9250ab21ce9d357e4f5d4cac4831ab994fb3cc6e
arnagaraja <arnagaraja@gmail.com>
2018-06-14 13:46:29 -0500

    Code for processing tweets and making the animation, as well as RDS file of data frame for 4 hour window

---
+++ makeAnimation.R
@@ -0,0 +1,78 @@
+library(ggplot2)
+library(animation)
+library(maps)
+
+statemap <- map_data("state")
+
+# By Second
+# saveGIF ({
+#       for(i in 1:nrow(tweets.by.second)) {
+#             
+#             # Add "cool down" for points using alpha
+#             if (i > 1) {
+#                   prev.match <- match
+#                   match <- tweets.by.second[i,1]
+#                   df <- filter(locations, time %in% c(match, prev.match)) %>%
+#                         arrange(time)
+#                   len.match <- nrow(filter(df, time %in% match))
+#                   len.prevmatch <- nrow(filter(df, time %in% prev.match))
+#                   alphalist <- c(rep(.3,len.prevmatch), rep(1,len.match))
+#                   df <- ungroup(df) %>%
+#                         mutate(alphaVal = alphalist)
+#                   
+#             } else {
+#                   match <- tweets.by.second[i,1]
+#                   df <- filter(locations, time %in% match) %>% 
+#                         mutate(alphaVal = rep(1, n()))
+#             }
+#             
+#             p1 <- ggplot(data = statemap, aes(x = long, y = lat, group = group)) +
+#                   geom_polygon(fill = "#55acee", color = "white") + 
+#                   coord_fixed(1.3, xlim = c(-130,-60), ylim = c(20,50)) + 
+#                   geom_point(data = df, aes(x = place_lon, y = place_lat), 
+#                              alpha = df$alphaVal, size = 4, inherit.aes = FALSE, 
+#                              color = "#292f33") + 
+#                   annotate("text", label = df$time[1], x = -95, y = 20, size = 8) + 
+#                   theme_void() + 
+#                   theme(legend.position = "none")
+#             
+#             print(p1)
+#       }
+# }, interval = 0.02, ani.width = 1028, ani.height = 514, movie.name = "animate-tweets-600s.gif")
+
+# Go through each individual time and make a data frame of each tweet. Then plot the location of that tweet in the US.
+
+saveGIF ({
+      for(i in 1:nrow(tweets.by.minute)) {
+            
+            # Add "cool down" for points using alpha
+            if (i > 1) {
+                  prev.match <- match
+                  match <- tweets.by.minute[i,1]
+                  df <- filter(locations, time.by.min %in% c(match, prev.match)) %>%
+                        arrange(time.by.min)
+                  len.match <- nrow(filter(df, time.by.min %in% match))
+                  len.prevmatch <- nrow(filter(df, time.by.min %in% prev.match))
+                  alphalist <- c(rep(.2,len.prevmatch), rep(.7,len.match))
+                  df <- ungroup(df) %>%
+                        mutate(alphaVal = alphalist)
+                  
+            } else {
+                  match <- tweets.by.minute[i,1]
+                  df <- filter(locations, time.by.min %in% match) %>% 
+                        mutate(alphaVal = rep(1, n()))
+            }
+            
+            p1 <- ggplot(data = statemap, aes(x = long, y = lat, group = group)) +
+                  geom_polygon(fill = "#55acee", color = "white") + 
+                  coord_fixed(1.3, xlim = c(-130,-60), ylim = c(20,50)) + 
+                  geom_point(data = df, aes(x = place_lon, y = place_lat), 
+                             alpha = df$alphaVal, size = 3, inherit.aes = FALSE, 
+                             color = "#292f33") + 
+                  annotate("text", label = df$time.by.min[1], x = -95, y = 20, size = 8) + 
+                  theme_void() + 
+                  theme(legend.position = "none")
+            
+            print(p1)
+      }
+}, interval = 0.25, ani.width = 1028, ani.height = 514, movie.name = "sunrise-tweets-7h.gif")
---
+++ processTweets.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(lubridate)
+library(streamR)
+
+# Coordinates for USA
+usa <- c(-124.848974,24.396308,66.53076,49.23037)
+filterStream(file.name = "dat/sunriseTrack.json", oauth = my_oauth, locations = usa, 
+             timeout = 25200) # 7 hours
+parsed <- parseTweets("dat/sunriseTrack-7h.json")
+parsed <- tbl_df(parsed)
+saveRDS(parsed, "dat/sunriseTweetsDF-7h.rds")
+
+# Grab the columns we want, filter out any NA entries, and do some mutations to get the structure we want
+locations <- select(parsed, created_at, place_lat, place_lon, lat, lon) %>% 
+      filter(!is.na(created_at)) %>%
+      mutate(created_at = as.POSIXct(created_at, 
+                                     format = "%a %b %d %H:%M:%S %z")) %>%
+      rename(time = created_at) %>%
+      mutate(time.by.min = format(time, 
+                                  format = "%Y-%m-%d %H:%M %Z", tz = "America/New_York"))
+
+# Some of the place_lat and place_lon values are NA, but there are entries for lat and lon. So, copy the lat/lon values to place_lat and place_lon
+for (i in 1:nrow(locations)) {
+      if (is.na(locations$place_lat[i]) & is.na(locations$place_lon[i])) {
+            locations$place_lat[i] <- locations$lat[i]
+            locations$place_lon[i] <- locations$lon[i]
+      }
+}
+
+# Get the number of tweets for each second; drop the first entry because it's usually not a full second
+# tweets.by.second <- group_by(locations, time) %>%
+#       summarize(entries = table(time)) %>%
+#       arrange(time) %>%
+#       slice(2:n())
+
+# Get the number of tweets for each minute
+
+tweets.by.minute <- group_by(locations, time.by.min) %>%
+      summarize(entries = table(time.by.min)) %>%
+      arrange(time.by.min) %>%
+      slice(2:n())
